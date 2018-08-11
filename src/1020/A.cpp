dec23a34d116c8a547be3079477096d27ac90967
Rui Gonçalves <ruippeixotog@gmail.com>
2018-08-11 18:00:46 +0100

    Clean up solutions to 1020A, 1020B and 1020C

--- src/1020/A.cpp
+++ src/1020/A.cpp
@@ -1,28 +1,12 @@
-#include <algorithm>
 #include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
-#include <vector>
-
-#define MAXN 200000
-#define INF 0x3f3f3f3f
-
-using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-
-int a[MAXN];
+#include <cstdlib>
 
 int main() {
-  int n, h, a, b, k; scanf("%d %d %d %d %d\n", &n, &h, &a, &b, &k);
+  int n, h, a, b, k;
+  scanf("%d %d %d %d %d\n", &n, &h, &a, &b, &k);
   while(k--) {
-    int ta, fa, tb, fb; scanf("%d %d %d %d\n", &ta, &fa, &tb, &fb);
+    int ta, fa, tb, fb;
+    scanf("%d %d %d %d\n", &ta, &fa, &tb, &fb);
 
     int cost = 0;
     if(ta != tb) {
--- src/1020/C.cpp
+++ src/1020/C.cpp
@@ -1,13 +1,6 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
-#include <vector>
 #include <numeric>
 
 #define MAXN 3000
@@ -17,13 +10,19 @@
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 
 int n, m;
 int p[MAXN], c[MAXN];
 
-int order[MAXN], votes[MAXM];
-int currVotes[MAXN]; bool bought[MAXN];
+int order[MAXN], votes[MAXM], currVotes[MAXN];
+bool bought[MAXN];
+
+int buyVote(int k) {
+  currVotes[0]++;
+  currVotes[p[k]]--;
+  bought[k] = true;
+  return c[k];
+}
 
 ll calcCost(int numVotes) {
   memcpy(currVotes, votes, sizeof(votes));
@@ -32,32 +31,18 @@ ll calcCost(int numVotes) {
   ll cost = 0;
   for(int i = 0; i < n && currVotes[0] < numVotes; i++) {
     if(currVotes[p[order[i]]] >= numVotes) {
-      cost += c[order[i]];
-      currVotes[0]++;
-      currVotes[p[order[i]]]--;
-      bought[order[i]] = true;
+      cost += buyVote(order[i]);
     }
   }
-
-  for(int i = 0; i < n && currVotes[0] < numVotes; i++) {
-    if(p[order[i]] != 0 && !bought[order[i]]) {
-      cost += c[order[i]];
-      currVotes[0]++;
-      currVotes[p[order[i]]]--;
-      bought[order[i]] = true;
-    }
-  }
-
   for(int i = 1; i < m; i++) {
     if(currVotes[i] >= numVotes) {
-      cerr << "cost for " << numVotes << ": fail0" << endl;
       return INF;
     }
   }
-  if(currVotes[0] < numVotes) {
-    cerr << "cost for " << numVotes << ": fail1" << endl;
-  } else {
-    cerr << "cost for " << numVotes << ": " << cost << endl;
+  for(int i = 0; i < n && currVotes[0] < numVotes; i++) {
+    if(p[order[i]] != 0 && !bought[order[i]]) {
+      cost += buyVote(order[i]);
+    }
   }
   return currVotes[0] < numVotes ? INF : cost;
 }
@@ -73,26 +58,13 @@ int main() {
   for(int i = 0; i < n; i++) {
     votes[p[i]]++;
   }
-
   iota(order, order + n, 0);
-  sort(order, order + n, [](int i1, int i2) {
-    return c[i1] < c[i2];
-  });
+  sort(order, order + n, [](int i1, int i2) { return c[i1] < c[i2]; });
 
   ll bestCost = INF;
   for(int numVotes = 0; numVotes <= n / 2 + 1; numVotes++) {
     bestCost = min(bestCost, calcCost(numVotes));
   }
-
-//  int minV = 0, maxV = n / 2 + 1;
-//  while(minV < maxV) {
-//    int midV = (minV + maxV) / 2;
-//    if(calcCost(midV) == -1) {
-//      minV = midV + 1;
-//    } else {
-//      maxV = midV;
-//    }
-//  }
   printf("%lld\n", bestCost);
   return 0;
 }
